4c71bee574c8c5b53ee178dd243935124c70055d
snatzehua <snatzehua@gmail.com>
2025-02-03 12:24:28 +0800

    kieran/home-foundation

--- my-portfolio/src/app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
-    </div>
-  );
-}
--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,101 +1,52 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="scroll-view">
+      <div className="base-screen">
+        <div className="image-container">
+          <img
+            className="mask-layer"
+            src="/images/background-mask-layer.png"
+            alt="Mask Image"
+          ></img>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="section-wrapper">
+        <div className="timeline">
+          <div className="timeline-item">
+            <img src="/images/RI-logo-circle.png"></img>
+            <div className="content">
+              <h2>Raffles Institution (IP)</h2>
+              <h3>GCE 'A'-levels (2015-2020)</h3>
+              <p>83.75 RP - AAB/B</p>
+              <p>Raffles Diploma - Distinction</p>
+              <p>Integrated Programme Y1-6 PCME</p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <img src="/images/MINDEF-logo-circle.png"></img>
+            <div className="content">
+              <h2>National Service (Joint Service)</h2>
+              <h3>Regular Service / Full-time NS (2021 - 2022)</h3>
+              <p>BSOM Award</p>
+              <p>Airforce Recruitment Centre, Pilot & WSO Recruitment</p>
+              <p>Officer Cadet - Pilot Trainee</p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <img src="/images/NUS-logo-circle.png"></img>
+            <div className="content">
+              <h2>National University of Singapore (NUS)</h2>
+              <h3>BSC (Hons) in Business Analytics (2023 - Present)</h3>
+              <p>4.48/5.00 CAP</p>
+              <p>Double Speciailisation in Machine Learning & Finance</p>
+              <p>Teaching Assistant for CS1010 - Programming Methodology</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section-wrapper">
+        <div className="pattern-bg"></div>
+      </div>
     </div>
   );
 }
